ca31baddf74fa735722cf8a8a7ef1862f37a4032
ifouzezi <ifoghaleuzezi@gmail.com>
2022-12-08 09:30:56 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Description: Prints specified text
+ * Description: prints "Programming is like building a multilingual puzzle"
  *
- * Return: 0 always on Success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
