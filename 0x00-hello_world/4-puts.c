b6cdfabab66c55b03e66f93deb8d8fef6c658241
ifouzezi <ifoghaleuzezi@gmail.com>
2022-12-08 09:20:15 +0100

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints specified text
+ * Return: 0 always on Sucess
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
