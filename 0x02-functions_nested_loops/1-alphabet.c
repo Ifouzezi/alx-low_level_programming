9e26869ba40e2ac5041e1bc5ae88d79c79cdb288
ifouzezi <ifoghaleuzezi@gmail.com>
2022-12-13 08:20:46 +0100

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabets
+ *
+ * Description: prints alphabets in lower case
+ * Return: 0 if successful
+ */
+void print_alphabet(void)
+{
+    int i;
+
+    i = 97;
+    while (i < 123) {
+        _putchar(i);
+        i++;
+    }
+    _putchar(10);
+}
