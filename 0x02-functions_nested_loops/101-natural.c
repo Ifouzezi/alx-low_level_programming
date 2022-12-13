0d5086710c343b4101df1eaed8d2d2dfeabfabce
ifouzezi <ifoghaleuzezi@gmail.com>
2022-12-13 13:03:37 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,82 +1,30 @@
+#include <stdio.h>
 #include "main.h"
 /**
- * print_digits - print each digit in a number
- * @tmp: integer value
- *
- * Returns: Always 0
+ * main - computes and prints the sum of all the multiples
+ * of 3 or 5 below 1024
+ * Return: Always 0 (Success)
  */
-void print_digits(int tmp)
+int main(void)
 {
-	int unit, tens, huns;
+	unsigned long int sum3, sum5, sum;
+	int i;
 
-	if (tmp > 99)
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
+
+	for (i = 0; i < 1024; ++i)
 	{
-		unit = tmp % 10;
-		tens = ((tmp - unit) % 100) / 10;
-		huns = (tmp - tens * 10 - unit) / 100;
-		_putchar('0' + huns);
-		_putchar('0' + tens);
-		_putchar('0' + unit);
-	}
-	else
-	{
-		if (tmp > 9)
+		if ((i % 3) == 0)
 		{
-			unit = tmp % 10;
-			tens = (tmp - unit) / 10;
-			_putchar('0' + tens);
-			_putchar('0' + unit);
-		}
-		else
+			sum3 = sum3 + i;
+		} else if ((i % 5) == 0)
 		{
-			_putchar('0' + tmp);
+			sum5 = sum5 + i;
 		}
 	}
-}
-
-/**
- * print_times_table - print n times table
- * @i: integer between 0 and 15
- *
- * Description: prints n times table\
- * prints nothing if n < 0 or n > 15
- * Return:Always 0
- */
-void print_times_table(int i)
-{
-	int m, n, tmp;
-
-	for (m = 0; m <= i; m++)
-	{
-		if ((i < 0) || (i > 15))
-			break;
-		for (n = 0; n <= i; n++)
-		{
-			tmp = m * n;
-			print_digits(tmp);
-			if (n < i)
-			{
-				_putchar(44);
-				if (m * (n + 1) < 10)
-				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(32);
-				}
-				else
-				{
-					if (m * (n + 1) < 100)
-					{
-						_putchar(32);
-						_putchar(32);
-					}
-					else
-					{
-						_putchar(32);
-					}
-				}
-			}
-		}
-		_putchar('\n');
-	}
+	sum = sum3 + sum5;
+	printf("%lu\n", sum);
+	return (0);
 }
