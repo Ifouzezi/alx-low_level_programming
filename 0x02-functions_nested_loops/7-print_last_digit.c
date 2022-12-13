59a0c7f5b981932d2796824ed39ca08ae44b04fc
ifouzezi <ifoghaleuzezi@gmail.com>
2022-12-13 09:47:09 +0100

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of an input number
+ * @c: digit input
+ *
+ * Description:print absolute value of digit
+ * Return: 1 if successful, 0 otherwise
+ */
+int print_last_digit(int c)
+{
+	int val;
+
+	val = c % 10;
+	if (val < 0)
+		val *= -1;
+	_putchar(val + '0');
+	return (val);
+    
